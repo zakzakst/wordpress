295f99df9fdabb7a0fc1f393a0a74a95abb9b8ca
zakzakst <59270506+zakzakst@users.noreply.github.com>
2021-10-12 14:02:55 +0900

    ニュース・実績コンポーネントの作成

--- src/nunjucks/constants.js
+++ src/nunjucks/constants.js
@@ -6,3 +6,69 @@ exports.CONSTANTS = {
   APPLE_ICON: 'https://domain/path/to/icon',
   OG_IMAGE: 'https://domain/path/to/image',
 };
+
+exports.NEWS_ITEMS = [
+  {
+    img: 'https://bulma.io/images/placeholders/640x320.png',
+    alt: 'ニュース画像',
+    text: 'テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。',
+    link: '/news/detail.html',
+  },
+  {
+    img: 'https://bulma.io/images/placeholders/640x320.png',
+    alt: 'ニュース画像',
+    text: 'テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。',
+    link: '/news/detail.html',
+  },
+  {
+    img: 'https://bulma.io/images/placeholders/640x320.png',
+    alt: 'ニュース画像',
+    text: 'テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。',
+    link: '/news/detail.html',
+  },
+  {
+    img: 'https://bulma.io/images/placeholders/640x320.png',
+    alt: 'ニュース画像',
+    text: 'テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。',
+    link: '/news/detail.html',
+  },
+  {
+    img: 'https://bulma.io/images/placeholders/640x320.png',
+    alt: 'ニュース画像',
+    text: 'テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。',
+    link: '/news/detail.html',
+  },
+];
+
+exports.WORK_ITEMS = [
+  {
+    img: 'https://bulma.io/images/placeholders/640x320.png',
+    alt: '実績画像',
+    text: 'テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。',
+    link: '/works/detail.html',
+  },
+  {
+    img: 'https://bulma.io/images/placeholders/640x320.png',
+    alt: '実績画像',
+    text: 'テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。',
+    link: '/works/detail.html',
+  },
+  {
+    img: 'https://bulma.io/images/placeholders/640x320.png',
+    alt: '実績画像',
+    text: 'テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。',
+    link: '/works/detail.html',
+  },
+  {
+    img: 'https://bulma.io/images/placeholders/640x320.png',
+    alt: '実績画像',
+    text: 'テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。',
+    link: '/works/detail.html',
+  },
+  {
+    img: 'https://bulma.io/images/placeholders/640x320.png',
+    alt: '実績画像',
+    text: 'テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。',
+    link: '/works/detail.html',
+  },
+];
